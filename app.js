b4a7b65bb3432e583425d69fa9a90269c1e1bab1
tmrsich <thomrs24@bergen.org>
2023-02-16 10:04:52 -0500

    Required pool connection

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const logger = require("morgan");
 const app = express();
 const port = process.env.PORT || 8080;
 const db = require('./db/db_connection');
+const db = require('./db/db_pool');
 
 // Configure Express to use EJS
 app.set( "views",  __dirname + "/views");
