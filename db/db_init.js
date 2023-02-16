a64d2203d7bd4ff5fc672d23cea068c1b1a8a26d
tmrsich <thomrs24@bergen.org>
2023-02-16 17:57:41 -0500

    Converted DATE to VARCHAR(10)

--- db/db_init.js
+++ db/db_init.js
@@ -11,14 +11,14 @@ const create_itemtable_sql = `
         item_id INT NOT NULL AUTO_INCREMENT,
         class_name VARCHAR(45) NOT NULL,
         assignment_name VARCHAR(45) NOT NULL,
-        due_date DATE NOT NULL,
+        due_date VARCHAR(10) NOT NULL,
         priority_rating TINYINT NOT NULL,
         assignment_type VARCHAR(45),
         assignment_format VARCHAR(45),
         interest_level TINYINT NULL,
         relevance_level TINYINT NULL,
         description VARCHAR(200) NULL,
-        PRIMARY KEY (item_id)
+        PRIMARY KEY (item_id, assignment_name)
     );
 `
 db.execute(create_itemtable_sql);
@@ -30,10 +30,10 @@ const insert_itemtable_sql = `
     VALUES
         (?, ?, ?, ?, ?, ?, ?, ?, ?);
 `
-db.execute(insert_itemtable_sql, ["Web Apps", "Lab 1", "2023-01-06", "5", "Lab", "Practical", "8", "3", "Finish details table and styles"]);
-db.execute(insert_itemtable_sql, ["Theory of Knowledge", "Extended Essay", "2024-06-09", "1", "Essay", "Research", "5", "8", "Find sources and complete annotated bibliography"]);
-db.execute(insert_itemtable_sql, ["AP Calculus AB", "Optimization Test Retake", "2023-01-08", "10", "Test", "Written", "2", "2", "Study optimization and related rates (extra attention to formulas)"]);
-db.execute(insert_itemtable_sql, ["Web Apps", "Lab 4", "2023-02-05", "3", "Lab", "Practical", "10", "3", ""]);
+db.execute(insert_itemtable_sql, ["Web Apps", "Lab 1", "01-06-2023", "5", "Lab", "Practical", "8", "3", "Finish details table and styles"]);
+db.execute(insert_itemtable_sql, ["Theory of Knowledge", "Extended Essay", "06-09-2024", "1", "Essay", "Research", "5", "8", "Find sources and complete annotated bibliography"]);
+db.execute(insert_itemtable_sql, ["AP Calculus AB", "Optimization Test Retake", "01-08-2023", "10", "Test", "Written", "2", "2", "Study optimization and related rates (extra attention to formulas)"]);
+db.execute(insert_itemtable_sql, ["Web Apps", "Lab 4", "02-05-2023", "3", "Lab", "Practical", "10", "3", "sample description"]);
 
 // Reads the sample items inserted
 const read_itemtable_sql = "SELECT * FROM Item";
