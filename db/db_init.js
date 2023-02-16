31148a83d2c968fb24d673f0fadef10c2cf505a9
tmrsich <thomrs24@bergen.org>
2023-02-15 21:03:00 -0500

    Reconfigured to contain the order listed in the form

--- db/db_init.js
+++ db/db_init.js
@@ -11,12 +11,12 @@ const create_itemtable_sql = `
         item_id INT NOT NULL AUTO_INCREMENT,
         class_name VARCHAR(45) NOT NULL,
         assignment_name VARCHAR(45) NOT NULL,
-        assignment_type VARCHAR(45) NOT NULL,
-        assignment_format VARCHAR(45) NOT NULL,
         due_date VARCHAR(10) NOT NULL,
         priority_rating TINYINT NOT NULL,
-        interest_level TINYINT,
-        relevance_level TINYINT,
+        assignment_type VARCHAR(45),
+        assignment_format VARCHAR(45),
+        interest_level TINYINT NULL,
+        relevance_level TINYINT NULL,
         description VARCHAR(200) NULL,
         PRIMARY KEY (item_id)
     );
@@ -26,14 +26,14 @@ db.execute(create_itemtable_sql);
 // Adds some sample items
 const insert_itemtable_sql = `
     INSERT INTO Item
-        (class_name, assignment_name, assignment_type, assignment_format, due_date, priority_rating, interest_level, relevance_level, description)
+        (class_name, assignment_name, due_date, priority_rating, assignment_type, assignment_format, interest_level, relevance_level, description)
     VALUES
         (?, ?, ?, ?, ?, ?, ?, ?, ?);
 `
-db.execute(insert_itemtable_sql, ["Web Apps", "Lab 1", "Lab", "Practical", "01-06-2023", "5", "8", "3", "Finish details table and styles"]);
-db.execute(insert_itemtable_sql, ["Theory of Knowledge", "Extended Essay", "Essay", "Research", "06-09-2024", "1", "5", "8", "Find sources and complete annotated bibliography"]);
-db.execute(insert_itemtable_sql, ["AP Calculus AB", "Optimization Test Retake", "Test", "Written", "01-08-2023", "10", "2", "2", "Study optimization and related rates (extra attention to formulas)"]);
-db.execute(insert_itemtable_sql, ["Web Apps", "Lab 4", "Lab", "Practical", "02-05-2023", "3", "10", "3", ""]);
+db.execute(insert_itemtable_sql, ["Web Apps", "Lab 1", "01-06-2023", "5", "Lab", "Practical", "8", "3", "Finish details table and styles"]);
+db.execute(insert_itemtable_sql, ["Theory of Knowledge", "Extended Essay", "06-09-2024", "1", "Essay", "Research", "5", "8", "Find sources and complete annotated bibliography"]);
+db.execute(insert_itemtable_sql, ["AP Calculus AB", "Optimization Test Retake", "01-08-2023", "10", "Test", "Written", "2", "2", "Study optimization and related rates (extra attention to formulas)"]);
+db.execute(insert_itemtable_sql, ["Web Apps", "Lab 4", "02-05-2023", "3", "Lab", "Practical", "10", "3", ""]);
 
 // Reads the sample items inserted
 const read_itemtable_sql = "SELECT * FROM Item";
